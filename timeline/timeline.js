763982af29a672aa0ced3d281504639a9fce8fd4
PlayitLOUD73 <github@lukewelborn.com>
2021-12-02 14:54:40 -0500

    Added some information

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -5,7 +5,16 @@ function makeContentVisible(x) {
   if (x === 0) {
     document.getElementsByClassName("content-box")[0].innerHTML = `
     <h3>The Battle of Fulford Gate</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h3>When: 20 September 1066</h3>
+    <h3>Who:<\h3> 
+    <p>English led by the Earls Edwin and Morcar<\p>
+    <p>Norwegians led by Harald Hardrada<\p>
+    <p>There was approximately 5 to 8000 troops on both sides.
+    Harald placed his weaker troops on his flanks to face the English.
+    The English attacked first and formed a shield-wall, and were intially successful
+    against Harald's weaker flanks.
+    After a long battle, the English fled in defeat. Due to the placement of the English army,
+    in between a ditch and a swamp, the retreat was disasterous for the English.<\p>
     `;
   }
   if (x === 1) {
