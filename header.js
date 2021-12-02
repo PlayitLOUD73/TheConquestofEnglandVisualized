3d98816330a5c7139b037008230397b1d63d15dc
PlayitLOUD73 <github@lukewelborn.com>
2021-12-02 15:23:01 -0500

    Aded people placeholder and a few changes

--- header.js
+++ header.js
@@ -10,6 +10,7 @@ class Header extends HTMLElement {
                 <li><a href="/index.html">Home</a></li>
                 <li><a href="/map/map.html">Map</a></li>
                 <li><a href="/timeline/timeline.html">Timeline</a></li>
+                <li><a href="/people/people.html">People</a></li>
                 <li style="float:right;"><a href="/sources/sources.html">Sources</a></li>
             </ul>
         `;
--- timeline/timeline.js
+++ timeline/timeline.js
@@ -5,16 +5,22 @@ function makeContentVisible(x) {
   if (x === 0) {
     document.getElementsByClassName("content-box")[0].innerHTML = `
     <h3>The Battle of Fulford Gate</h3>
-    <h3>When: 20 September 1066</h3>
-    <h3>Who:<\h3> 
-    <p>English led by the Earls Edwin and Morcar<\p>
-    <p>Norwegians led by Harald Hardrada<\p>
+    <h4>When: 20 September 1066<br>Who: English vs. Norwegians</h4>
+    <p>The English were led by the Earls Edwin and Morcar.
+    The Norwegian vikings were led by King Harald of Norway</p>
     <p>There was approximately 5 to 8000 troops on both sides.
     Harald placed his weaker troops on his flanks to face the English.
-    The English attacked first and formed a shield-wall, and were intially successful
-    against Harald's weaker flanks.
+    The weaker troops were led by Tostig.
+    </p> 
+    <p>The English attacked first and formed a shield-wall, and were intially successful
+    against Harald's weaker flanks. 
     After a long battle, the English fled in defeat. Due to the placement of the English army,
-    in between a ditch and a swamp, the retreat was disasterous for the English.<\p>
+    in between a ditch and a swamp, the retreat was disasterous for the English. Jordan Poss wrote
+    on the retreat, "Harald pressed his army against the fleeing English and butchered them" (53).
+    The English had no where to go and Harald took advantage of the situation. The initial placement
+    of the English army, while successful at preventing flanking maneuvers, it left the English with
+    no where go when the retreat started.
+    </p>
     `;
   }
   if (x === 1) {
