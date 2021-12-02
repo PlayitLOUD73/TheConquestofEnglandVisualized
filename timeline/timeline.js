fd0447dc7c1de818514aebeeaa665bd83e6ec20e
PlayitLOUD73 <github@lukewelborn.com>
2021-12-02 14:29:59 -0500

    Added multiple button support

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -1,4 +1,17 @@
-function makeContentVisible() {
+function makeContentVisible(x) {
   document.getElementsByClassName("content-box")[0].style.visibility =
     "visible";
+
+  if (x === 0) {
+    document.getElementsByClassName("content-box")[0].innerHTML = `
+    <h3>The Battle of Fulford Gate</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    `;
+  }
+  if (x === 1) {
+    document.getElementsByClassName("content-box")[0].innerHTML = `
+    <h3>The Battle of Stamford Bridge</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    `;
+  }
 }
