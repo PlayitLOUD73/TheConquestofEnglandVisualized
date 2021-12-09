d0af2e3c394c45767dbc3e8923c0ff354153eadf
PlayitLOUD73 <github@lukewelborn.com>
2021-12-09 02:50:17 -0500

    More changes. Added Hastings map

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -1,13 +1,10 @@
 function makeContentVisible(x) {
-  document.getElementsByClassName("content-box")[0].style.visibility =
-    "visible";
-
   if (x === 0) {
     document.getElementsByClassName("content-box")[0].innerHTML = `
     <h3>The Battle of Fulford Gate</h3>
     <h4>When: 20 September 1066<br>Who: English vs. Norwegians</h4>
     <p>The English were led by the Earls Edwin and Morcar.
-    The Norwegian vikings were led by King Harald of Norway.</p>
+    The Norwegian vikings were led by King Harald of Norway with help from Tostig.</p>
     <p>There were approximately 5 to 8000 troops on both sides.
     Harald placed his weaker troops on his flanks to face the English.
     The weaker troops were led by Tostig.
@@ -26,12 +23,23 @@ function makeContentVisible(x) {
   if (x === 1) {
     document.getElementsByClassName("content-box")[0].innerHTML = `
     <h3>The Battle of Stamford Bridge</h3>
+    <h4>When: <br>Who: English vs. Norwegians</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     `;
   }
   if (x == 2) {
     document.getElementsByClassName("content-box")[0].innerHTML = `
     <h3>The Battle of Hastings</h3>
+    <h4>When: 14 October 1066<br>Who: English vs. Normans</h4>
+    <p>The English were led by King Harold of England.
+    The Norweigans were led by King Harald of Norway with help from Tostig.</p>
+    <p>The English held the high ground in this battle. For most of the battle, the English were able to 
+        hold the line against the Norman infantry and calvary. The Normans would retreat and then turn on
+        the English that pursued them. The battle turned when Harold was fatally injured. Jordan Poss wrote,
+        "... a Norman arrow sailed out of the evening sky and pierced his eye and sank through the eye socket
+        into the base of his brain" (97).</p>
+    <p>The injury and death of Harold caused chaos in the English army and eventually allowed the Norman
+        calvray to mop up the remaining soldiers (Poss 99). </p>
 
 
     <p>For more strategy about the battle and a map, <a href="/map/mapHastings.html">click here.</a></p>
