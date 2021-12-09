9a672f323ad8b1b1a8342986fca78394460a3cd4
PlayitLOUD73 <github@lukewelborn.com>
2021-12-08 23:42:43 -0500

    Link fix

--- header.js
+++ header.js
@@ -7,20 +7,20 @@ class Header extends HTMLElement {
     this.innerHTML = `
             <!-- Site Navigation Menu-->
             <div class="navbar">
-            <a href="TheConquestofEnglandVisualized/index.html">Home</a>
+            <a href="/index.html">Home</a>
             <div class="dropdown">
               <button class="dropbtn">Maps
                 <i class="fa fa-caret-down"></i>
               </button>
               <div class="dropdown-content">
-                <a href="TheConquestofEnglandVisualized/map/map1066.html">1066 Map</a>
+                <a href="/map/map1066.html">1066 Map</a>
                 <a href="#">Link 2</a>
                 <a href="#">Link 3</a>
               </div>
             </div> 
-            <a href="TheConquestofEnglandVisualized/timeline/timeline.html">Timeline</a>
-            <a href="TheConquestofEnglandVisualized/people/people.html">People</a>
-            <a style="float:right" href="TheConquestofEnglandVisualized/sources/sources.html">Sources</a>
+            <a href="/timeline/timeline.html">Timeline</a>
+            <a href="/people/people.html">People</a>
+            <a style="float:right" href="/sources/sources.html">Sources</a>
           </div>
         `;
   }
