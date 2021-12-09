40aa867b857484e0f901d5411eb39152ab584dd5
PlayitLOUD73 <github@lukewelborn.com>
2021-12-08 23:22:28 -0500

    Lots of website changes including a map page

--- header.js
+++ header.js
@@ -6,13 +6,22 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <!-- Site Navigation Menu-->
-            <ul class="navbar">
-                <li><a href="/index.html">Home</a></li>
-                <li><a href="/map/map.html">Map</a></li>
-                <li><a href="/timeline/timeline.html">Timeline</a></li>
-                <li><a href="/people/people.html">People</a></li>
-                <li style="float:right;"><a href="/sources/sources.html">Sources</a></li>
-            </ul>
+            <div class="navbar">
+            <a href="/index.html">Home</a>
+            <div class="dropdown">
+              <button class="dropbtn">Map
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="/map/map1066.html">1066 Map</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div> 
+            <a href="/timeline/timeline.html">Timeline</a>
+            <a href="/people/people.html">People</a>
+            <a style="float:right" href="/sources/sources.html">Sources</a>
+          </div>
         `;
   }
 }
